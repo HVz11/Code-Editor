5b303fb608b0721f1ba202b4df6fdfb7dd0cf585
Vaibhav <vs630916@gmail.com>
2024-04-14 00:30:54 +0530

    Refactor CodeMirror

--- codify/src/components/Editor.js
+++ codify/src/components/Editor.js
@@ -9,38 +9,37 @@ import "codemirror/addon/edit/closebrackets";
 
 function Editor({ socketRef, roomId, onCodeChange }) {
   const editorRef = useRef(null);
+
   useEffect(() => {
-    const init = async () => {
-      const editor = CodeMirror.fromTextArea(
-        document.getElementById("realtimeEditor"),
-        {
-          mode: { name: "javascript", json: true },
-          theme: "dracula",
-          autoCloseTags: true,
-          autoCloseBrackets: true,
-          lineNumbers: true,
-        }
-      );
-      // for sync the code
-      editorRef.current = editor;
+    const editor = CodeMirror.fromTextArea(
+      document.getElementById("realtimeEditor"),
+      {
+        mode: { name: "javascript", json: true },
+        theme: "dracula",
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      }
+    );
 
-      editorRef.current.on("change", (instance, changes) => {
-        const { origin } = changes;
-        const code = instance.getValue(); 
-        onCodeChange(code);
-        if (origin !== "setValue") {
-          socketRef.current.emit(ACTIONS.CODE_CHANGE, {
-            roomId,
-            code,
-          });
-        }
-      });
+    editorRef.current = editor;
+
+    editor.on("change", (instance, changes) => {
+      const { origin } = changes;
+      const code = instance.getValue();
+      onCodeChange(code);
+      if (origin !== "setValue") {
+        socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+          roomId,
+          code,
+        });
+      }
+    });
+    return () => {
+      editor.toTextArea();
     };
-
-    init();
   }, []);
 
-  // data receive from server
   useEffect(() => {
     if (socketRef.current) {
       socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
